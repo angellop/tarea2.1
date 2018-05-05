79a394df554becd0ccb5d19451d69d8131de217f
angellop <39015208+angellop@users.noreply.github.com>
2018-05-05 17:43:38 -0600

    Add files via upload

---
+++ src/ejercicios2/Ejercicios2.java
@@ -0,0 +1,88 @@
+package ejercicios2;
+
+class Rectangle {
+
+    int lenght;
+    int width;
+
+    Rectangle(int lenght, int width) {
+
+        this.lenght = lenght;
+        this.width = width;
+    }
+
+    int getLength() {
+        int getLength;
+        return getLength = lenght;
+
+    }
+
+    int getWidth() {
+        int getWidth;
+        return getWidth = width;
+
+    }
+
+    void setLength(int setLength) {
+        this.lenght = setLength;
+    }
+
+    void setWidth(int setWidth) {
+        this.width = setWidth;
+    }
+
+    int getArea() {
+        int area = lenght*width;
+        return area;
+    }
+
+    int getPerimeter() {
+
+        int perimetro;
+        return perimetro = 2 * (lenght) + width;
+    }
+
+}
+
+public class Ejercicios2 {
+
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(10, 20);
+        Rectangle r2 = new Rectangle(100, 200);
+
+        System.out.print("get: ");
+        if (r1.getLength() == 10 && r1.getWidth() == 20
+                && r2.getLength() == 100 && r2.getWidth() == 200) {
+            System.out.println("pass");
+        } else {
+            System.out.println("fail");
+        }
+
+        System.out.print("set: ");
+        r1.setLength(2);
+        r1.setWidth(3);
+        r2.setLength(20);
+        r2.setWidth(30);
+        if (r1.getLength() == 2 && r1.getWidth() == 3
+                && r2.getLength() == 20 && r2.getWidth() == 30) {
+            System.out.println("pass");
+        } else {
+            System.out.println("fail");
+        }
+
+        System.out.print("area: ");
+        if (r1.getArea() == 6 && r2.getArea() == 600) {
+            System.out.println("pass");
+        } else {
+            System.out.println("fail");
+        }
+
+        System.out.print("perimeter: ");
+        if (r1.getPerimeter() == 10 && r2.getPerimeter() == 100) {
+            System.out.println("pass");
+        } else {
+            System.out.println("fail");
+        }
+    }
+
+}
